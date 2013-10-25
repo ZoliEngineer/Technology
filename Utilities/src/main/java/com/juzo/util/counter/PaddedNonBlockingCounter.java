2745d00e4117bd9059fc3cd471efc18c8446e336
ZoliEngineer <juzoli@gmail.com>
2013-10-25 11:11:56 +0200

    introducing padding in padded counter to avoid false sharing

--- Utilities/src/main/java/com/juzo/util/counter/PaddedNonBlockingCounter.java
+++ Utilities/src/main/java/com/juzo/util/counter/PaddedNonBlockingCounter.java
@@ -1,22 +1,25 @@
 package com.juzo.util.counter;
 
 public class PaddedNonBlockingCounter implements ConcurrentCounter {
-    private final int[] counter; // TODO: introduce padding in a new structure
+    private static final int PADDING_SIZE = 8;
+     private static final int IN_PADDING_POSIION = 4;
+    
+    private final int[][] counter;   // Simplest solution, needs analysis if a dedicated object with encapsulates padding performs better
 
     public NonBlockingCounter(int threadCount){
-        counter = new int[threadCount];
+        counter = new int[threadCount][PADDING_SIZE];
     }
 
     @Override
     public void increment(int callerId) {
-        counter[callerId]++; // TODO: not safe for ArrayIndexOutOfBoundsException
+        counter[callerId][IN_PADDING_POSIION]++; // TODO: not safe for ArrayIndexOutOfBoundsException
     }
 
     @Override
     public int get() {
         int aggregatedCounter = 0;
-        for(int i : counter){
-            aggregatedCounter += i;
+        for(int[] i : counter){
+            aggregatedCounter += i[IN_PADDING_POSIION];
         }
         return aggregatedCounter;
     }
